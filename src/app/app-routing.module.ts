09cd63ff471af70d838f7316324f31f378de2f4f
jcruzaguidev <jcruzagui@gmail.com>
2022-01-19 14:48:32 -0500

    Cambios en routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from './auth/login/login.component';
-import {RegisterComponent} from './auth/register/register.component';
-import {NopagefoundComponent} from './nopagefound/nopagefound.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {Grafica1Component} from './pages/grafica1/grafica1.component';
-import {PagesComponent} from './pages/pages.component';
-import {ProgressComponent} from './pages/progress/progress.component';
 
-const routes:Routes = [
-   {
-      path:'', 
-      component: PagesComponent,
-      children: [
-         {path:'dashboard', component: DashboardComponent},
-         {path:'progress', component: ProgressComponent},
-         {path:'grafica1', component: Grafica1Component},
-         {path:'', redirectTo:'/dashboard', pathMatch:'full'}
-      ]
-   },
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-   {path:'login', component: LoginComponent},
-   {path:'register', component: RegisterComponent},
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
-   {path:'**', component: NopagefoundComponent}
+const routes: Routes = [
+   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+   { path: '**', component: NopagefoundComponent }
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
