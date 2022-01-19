68ac2570b8e3aaa7480749c7327cd01561706af8
jcruzaguidev <jcruzagui@gmail.com>
2022-01-19 12:58:30 -0500

    Cambios menores de reubicación

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,14 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PagesComponent implements OnInit {
+  year = new Date().getFullYear();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
